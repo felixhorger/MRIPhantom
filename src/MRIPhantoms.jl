ab09be15c175cf6a254f844614fc33d251d08e33
Felix Horger <felix.horger@kcl.ac.uk>
2023-06-22 14:39:18 +0100

    Added functionality (phantoms, coils)

--- src/MRIPhantoms.jl
+++ src/MRIPhantoms.jl
@@ -13,13 +13,20 @@ module MRIPhantoms
 	function coil_sensitivities(shape::NTuple{N, Integer}, num_channels::NTuple{M, Integer}, width::Real) where {N, M}
 		@assert N == M + 1
 		# TODO: Phases?
-		sensitivities = zeros(ComplexF64, shape..., prod(num_channels))
+		total_num_channels = prod(num_channels)
+		sensitivities = Array{ComplexF64, N+1}(undef, shape..., total_num_channels)
 		width = width .* shape
+		centre = shape .÷ 2
+		mask_width = 0.4 .* shape
+		num_channels = (num_channels[1] - 1, num_channels[2:end]...)
 		for (c, Φ) = enumerate(hypersphere(Val(N), num_channels))
 			position = 0.25 .* shape .* (spherical2cartesian(Φ) .+ 2)
 			for X in CartesianIndices(shape) # TODO: inbounds
-				sensitivities[X, c] = @. exp(-0.5 * $sum((($Tuple(X) - position) / width)^2))
+				sensitivities[X, c] = exp(-0.5 * (sum(((Tuple(X) .- position) ./ width).^2 .+ ((Tuple(X) .- centre) ./ mask_width).^6)))
 			end
+		end
+		for X in CartesianIndices(shape) # TODO: inbounds
+			sensitivities[X, total_num_channels] = exp(-0.5 * sum(((Tuple(X) .- centre) ./ width).^2))
 		end
 		return sensitivities
 	end
@@ -46,10 +53,20 @@ module MRIPhantoms
 		return shutter
 	end
 
+	function stamp!(a::AbstractArray{<: Number, N}, mask::AbstractArray{Bool, N}, position::CartesianIndex{N}, v::Number) where N
+		position = position - CartesianIndex{N}(size(mask) .÷ 2)
+		for i in CartesianIndices(mask)
+			j = i + position
+			!checkbounds(Bool, a, j) && continue
+			a[j] += v * mask[i]
+		end
+		return a
+	end
+
 
 	"""
 		a is upsampled array
-		Last axis is channels
+		last axis is channels, dynamic etc
 		shape is size of target kspace
 	"""
 	function measure(
@@ -57,7 +74,7 @@ module MRIPhantoms
 		upsampling::NTuple{M, Integer},
 		shape::NTuple{M, Integer}
 	) where {N, M}
-		@assert N == M + 1
+		@assert M + 1 == N
 		upsampled_shape = size(a)[1:M]
 		@assert upsampled_shape == shape .* upsampling
 		# Get location of target k-space
@@ -65,17 +82,18 @@ module MRIPhantoms
 		# FFT and extract target k-space
 		kspace = fft(a, 1:M)
 		kspace = fftshift(kspace, 1:M)
-		kspace = @view kspace[centre_indices..., :]
-		kspace = ifftshift(kspace, 1:M)
+		kspace_centre = @view kspace[centre_indices..., :]
+		lowres_kspace = ifftshift(kspace_centre, 1:M)
 		# Normalise for different k-space sizes
-		kspace ./= prod(upsampling)
-		return kspace
+		lowres_kspace ./= prod(upsampling)
+		return lowres_kspace
 	end
 	for N ∈ 2:4
 		ks = join(("k[$d, :]" for d = 1:N-1), ", ")
 		@eval begin
 			"""
 				upsampling that _was_ applied to a
+				need to reshape a if channels and dynamic
 			"""
 			function measure(
 				a::AbstractArray{<: Number, $N},
@@ -92,13 +110,44 @@ module MRIPhantoms
 	end
 
 
-	function homogeneous(shape::NTuple{N, Integer}, upsampling::NTuple{N, Integer}) where N
+	function homogeneous(shape::NTuple{N, Integer}, upsampling::NTuple{N, Integer}; radius::Real=0.4) where N
 		upsampled_shape = upsampling .* shape
 		# Spatial profile of phantom
-		highres_shutter = ellipsoidal_shutter(upsampled_shape, 0.4 .* upsampled_shape)
+		highres_shutter = ellipsoidal_shutter(upsampled_shape, radius .* upsampled_shape)
 		# Downsample to get back to target resolution
 		shutter = MRIRecon.downsample(highres_shutter, upsampling)
-		return shutter, highres_shutter
+		return shutter
+	end
+
+	function nist_slice(
+		shape::NTuple{N, Integer};
+		intensities::AbstractVector{T}=ones(9)
+	) where {N, T <: Number}
+		@assert N == 2
+		# Spatial profile of phantom
+		phantom = zeros(T, shape)
+		body = ellipsoidal_shutter(shape, 0.4 .* shape)
+		centre = CartesianIndex(shape .÷ 2)
+		stamp!(phantom, body, centre, intensities[1])
+		circle = ellipsoidal_shutter(shape, 0.05 .* shape)
+		r = 0.275 .* shape # circles located on circle of this radius
+		for (i, ϕ) in enumerate(range(0, 2π; length=8+1)[1:8])
+			sine, cosine = sincos(ϕ)
+			position = CartesianIndex{2}(floor.(Int, r .* (cosine, sine))) + centre
+			stamp!(phantom, circle, position, intensities[i+1] .- 1.0)
+		end
+		return phantom
+	end
+
+	"""
+		dynamics[time, label]
+	"""
+	function dynamic_from_segmentation(labels::AbstractArray{<: Integer, N}, dynamics::AbstractMatrix{T}; outtype::Type=T) where {T, N}
+		phantom = Array{outtype, N+1}(undef, size(labels), size(dynamic, 1))
+		for I in CartesianIndices(labels)
+			@views phantom[I, :] = dynamics[:, labels[I]]
+		end
+		return phantom
 	end
 
 	function add_dynamic_dim(
@@ -107,8 +156,25 @@ module MRIPhantoms
 	) where N
 		# Broadcast spatial dimensions and channels
 		broadcasted_shape = (ntuple(_ -> 1, N)..., length(dynamic))
-		a = reshape(dynamic, broadcasted_shape) .* a
-		return a
+		return reshape(dynamic, broadcasted_shape) .* a
+	end
+
+	function add_dynamic_dim(
+		a::AbstractArray{T, N},
+		dynamics::AbstractMatrix{<: Number},
+		indices::AbstractArray{<: Integer, N}
+	) where {T <: Number, N}
+		# Broadcast spatial dimensions and channels
+		num_dynamic = size(dynamics, 1)
+		ad = Array{T, N+1}(undef, size(a)..., num_dynamic)
+		@views for i in CartesianIndices(indices)
+			if indices[i] == 0
+				ad[i, :] .= 0.0
+			else
+				ad[i, :] .= dynamics[:, indices[i]] .* a[i]
+			end
+		end
+		return ad
 	end
 
 end
